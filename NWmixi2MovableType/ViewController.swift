b7641deae67cfd4a9053d5ed6b7ad0dddb050868
Your Name <youexample.com>
2019-10-08 16:02:24 +0900

    Initial Commit

---
+++ NWmixi2MovableType/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NWmixi2MovableType
+//
+//  Created by M on 2019/10/08.
+//  Copyright © 2019 M. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
